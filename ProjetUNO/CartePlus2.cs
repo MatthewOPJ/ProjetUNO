c2c0f7e8b28b69c1357e8f218fb3ef51e7472134
Tolomas3721 <thomasbeaulieu3721@gmail.com>
2025-11-29 13:34:48 -0500

    ajout de la classe CartePlus2

---
+++ ProjetUNO/CartePlus2.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ProjetUNO
+{
+    internal class CartePlus2 : Carte
+    {
+        public override void Jouer(ref Jeu jeu){
+
+        }
+
+        public override bool PeutJouer(Carte topPile){
+
+        }
+
+        public override void Afficher(){
+
+        }
+    }
+}
--- ProjetUNO/obj/Debug/net8.0/ProjetUNO.AssemblyInfo.cs
+++ ProjetUNO/obj/Debug/net8.0/ProjetUNO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetUNO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ed5deb44fcbfb7fd3b116aa9673ab613faa4f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bceeb84a6cef3b3c4b317688453701cd9817fb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetUNO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetUNO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
